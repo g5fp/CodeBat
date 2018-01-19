fd6a2e624c765c39707b44172b43f27fcfe9d5ff
Rob <Rob@DELL>
2018-01-19 17:29:23 -0600

    First Push

---
+++ CodeBat/src/codebat/CodeBat.java
@@ -0,0 +1,57 @@
+package codebat;
+
+// Class for Code Bat examples
+public class CodeBat {
+
+	// boolean bool;
+	// String str;
+	// int num;
+	// String[] array;
+
+	public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+		if (aSmile == true && bSmile == true) {
+			return true;
+		}
+		if (aSmile == false && bSmile == false) {
+			return true;
+		}
+		if (aSmile == true && bSmile == false) {
+			return false;
+		}
+		return false;
+	}
+
+	public int sumDouble(int a, int b) {
+		if (a == b) {
+			return (a + b) * 2;
+		}
+		return a + b;
+	}
+
+	public String delDel(String str) {
+		if (str.length() < 4) {
+			return str;
+		}
+		if (str.substring(1, 4).equals("del")) {
+			str = str.replaceFirst("del", "");
+
+			return str;
+		}
+
+		return str;
+	}
+
+	public boolean mixStart(String str) {
+		if (str.length() < 3)
+			return false;
+
+		if (str.substring(1, 3).equals("ix"))
+			return true;
+
+		return false;
+	}
+	public String startOz(String str) {
+		  return str;
+	}
+
+}
---
+++ CodeBat/src/codebat/CodeBatTester.java
@@ -0,0 +1,27 @@
+package codebat;
+
+public class CodeBatTester
+{
+
+   public static void main(String[] args)
+   {
+
+      CodeBat codebat = new CodeBat();
+      // boolean result = codebat.monkeyTrouble(true, true);
+      // System.out.println(result);
+      // int num = codebat.sumDouble(2, 2);
+      // System.out.println(num);
+       dosomething();
+      // String str = codebat.delDel("adelHello");
+//      boolean bool = codebat.mixStart("mix snakes");
+      String str = codebat.startOz("ozymandias");
+      System.out.println(str);
+
+   }
+
+   private static void dosomething()
+   {
+//      System.out.println("I'm doing something");
+   }
+
+}
