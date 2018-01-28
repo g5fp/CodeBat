185f2c687a09567cd865da64766829bdcd5ed28d
Rob <Rob@DELL>
2018-01-27 19:58:31 -0600

    ???

--- CodeBat/src/codebat/CodeBatTester.java
+++ CodeBat/src/codebat/CodeBatTester.java
@@ -1,27 +1,24 @@
 package codebat;
 
-public class CodeBatTester
-{
+public class CodeBatTester {
 
-   public static void main(String[] args)
-   {
+	public static void main(String[] args) {
 
-      CodeBat codebat = new CodeBat();
-      // boolean result = codebat.monkeyTrouble(true, true);
-      // System.out.println(result);
-      // int num = codebat.sumDouble(2, 2);
-      // System.out.println(num);
-       dosomething();
-      // String str = codebat.delDel("adelHello");
-//      boolean bool = codebat.mixStart("mix snakes");
-      String str = codebat.startOz("pana");
-      System.out.println(str);
+		CodeBat codebat = new CodeBat();
+		// boolean result = codebat.monkeyTrouble(true, true);
+		// System.out.println(result);
+		// int num = codebat.sumDouble(2, 2);
+		// System.out.println(num);
+		dosomething();
+		// String str = codebat.delDel("adelHello");
+		boolean bool = codebat.mixStart("mix snakes");
+		// String str = codebat.startOz("pan");
+		System.out.println(bool);
 
-   }
+	}
 
-   private static void dosomething()
-   {
-//      System.out.println("I'm doing something");
-   }
+	private static void dosomething() {
+		// System.out.println("I'm doing something");
+	}
 
 }
